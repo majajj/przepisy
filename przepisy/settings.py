434ab4095d07bc09e0dbe053537220b9966cee4b
Marysia <maja_jj@interia.pl>
2024-01-11 16:51:47 +0100

    added redirect after login

--- przepisy/settings.py
+++ przepisy/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'all_recipies'
+LOGOUT_REDIRECT_URL = 'login'
