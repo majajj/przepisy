e57d99fe83557ef9cd39f9115618c46e10e24be2
Marysia <maja_jj@interia.pl>
2024-01-11 14:23:47 +0100

    changes in settings

--- przepisy/settings.py
+++ przepisy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-uhf-#7ueu_!lby9o_b-l%m^)l2dz^i%-42*9-gp7jd43r$uk'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+    # 'django-insecure-6-uhf-#7ueu_!lby9o_b-l%m^)l2dz^i%-42*9-gp7jd43r$uk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -82,7 +84,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
+# 'postgres://przepisy_django_render_user:FaNm0VkrTrPbTkUSPfDYBaSW7AiGC17L@dpg-cmfu9beg1b2c73cpuk10-a.oregon-postgres.render.com/przepisy_django_render
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
